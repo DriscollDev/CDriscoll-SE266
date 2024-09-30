587d8ca57a634b766911754f55e9a28cd9d92546
DriscollDev <cdriscolldev@gmail.com>
2024-09-30 10:15:59 -0400

    Added navigation to help get to assigments

--- w1/index.php
+++ w1/index.php
@@ -6,7 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>This is html</h1>
-    <p><?php echo "this is php" ?></p>
+    <h1>Week 1 Assignments</h1>
+
+    <ul>
+        <li><a href="arrays.php">Arrays</a></li>
+        <li><a href="assarrays.php">Assosiative Arrays</a></li>
+        <li><a href="conditionals.php">Conditionals</a></li>
+        <li><a href="functions.php">Functions</a></li>
+        <li><a href="fizzbuzz.php">FizzBuzz</a></li>
+
+    </ul>
 </body>
 </html>
