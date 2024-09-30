cf16397576b5aefc48c1086b821f11805964b291
DriscollDev <cdriscolldev@gmail.com>
2024-09-30 09:35:27 -0400

    Mini-Task D

---
+++ w1/assarrays.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<!-- This is a simple PHP script to display a task -->
+<body>
+    <?php
+    $task = [
+        'title' => 'Finish homework',
+        'due' => 'today',
+        'assigned to' => 'John'
+    ];
+    $title = 'Task';
+    ?>
+    <h1><?php echo $title; ?></h1>
+    <ul>   
+        <?php foreach ($task as $key => $value) { ?>
+            <li><strong><?php echo ($key); ?></strong> <?php echo $value; ?> </li>    
+        <?php } ?>
+</body>
+</html>
